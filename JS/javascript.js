0763358792f9c84ebb4c7ed2763d50af663a41ac
unknown <luangie982@hotmail.com>
2021-01-26 19:57:56 -0500

    creacion de alerta2

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,4 +1,6 @@
 //( "¡Bienvenido, como puedo ayudarte")
 swal("¡Bienvenido!" , "...¡Como puedo ayudarte!");
 
+//("Buen trabajo, excelente, felicitaciones")
+swal ("¡Buen trabajo!", "¡excelente!", "felicitaciones");
 
